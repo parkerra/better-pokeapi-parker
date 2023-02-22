e6c7fab64584943cfc709f19ecbb44e6b512a5ca
Parker Abramson <parkerrabramson@gmail.com>
2023-02-21 20:29:55 -0500

    Start changing front end for Pokemon Name page

--- src/pages/name.js
+++ src/pages/name.js
@@ -4,12 +4,13 @@ import Link from 'next/link'
 
 const fetcher = async (url) => {
     const res = await axios.get(url)
-    console.log(res.data)
     return res.data
 }
 
 export default function Name() {
-    let { data, error, isLoading, isValidating } = useSWR("/api/pokemon/pikachu", fetcher)
+    let pokemon = "pikachu"
+    // let { data, error, isLoading, isValidating } = useSWR("/api/pokemon/pikachu", fetcher)
+    let { data, error, isLoading, isValidating } = useSWR("/api/pokemon/" + pokemon, fetcher)
 
     if (isLoading) return <div>Loading</div>
     if (!data) return (
@@ -21,10 +22,15 @@ export default function Name() {
 
     let { pokemonName, sprite, types } = data
 
-
     return (
         <>
+            {/* {console.log("SOMETHING IS HAPPENING")} */}
             <h1><Link href="/">Better PokeAPI</Link></h1>
+            <input type="text" id="pokemonInput" name="name" />
+            {/* <h2>{pokemonInput.value}</h2> */}
+            {/* {console.log("SOMETHING IS HAPPENING")} */}
+            {/* {console.log(pokemon)} */}
+            {/* {pokemon = pokemonInput.pokemon} */}
             {isValidating ? (
                 <h2>Validating</h2>
             ) : (
